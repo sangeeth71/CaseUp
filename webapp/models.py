4f8a8874b850ac94046426b507a5bd5a1d22e898
sangeeth71 <sangeethc629@gmail.com>
2025-11-13 10:09:04 +0530

    created cartDB

--- webapp/models.py
+++ webapp/models.py
@@ -10,3 +10,13 @@ class MessageDb(models.Model):
     Name=models.CharField(max_length=100,null=True,blank=True)
     Email=models.EmailField(max_length=100, null=True, blank=True)
     Message=models.CharField(max_length=600,null=True,blank=True)
+class CartDB(models.Model):
+    Username = models.CharField(max_length=100, null=True, blank=True)
+    CoverName = models.CharField(max_length=100, null=True, blank=True)
+    Quantity = models.IntegerField(null=True, blank=True)
+    Price = models.IntegerField(null=True, blank=True)
+    Total_Price = models.IntegerField(null=True, blank=True)
+    Case_Image = models.ImageField(upload_to="Cart_Images", null=True, blank=True)
+
+    def __str__(self):
+        return self.CoverName
