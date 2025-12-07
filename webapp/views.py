b09032fc9ad94700cfdbf169558ac65f7c875307
sangeeth71 <sangeethc629@gmail.com>
2025-12-08 00:26:54 +0530

    checkout

--- webapp/views.py
+++ webapp/views.py
@@ -46,13 +46,12 @@ def contact_page(request):
 
 def cart_page(request):
 
-    cases=CartDB.objects.filter(Username= request.session['username'])
-
+    cases=CartDB.objects.filter(Username=request.session['username'])
     # count in cart icon
     cart_count = 0
     uname = request.session.get('username')
     if uname:
-        cart_count = CartDB.objects.filter(Username=request.session['username']).count
+        cart_count = CartDB.objects.filter(Username=request.session('username')).count
 
     # calculating total amount
     sub_total=0
